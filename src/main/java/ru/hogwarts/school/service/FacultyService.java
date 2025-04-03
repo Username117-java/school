c342338046822fee675a1af910a9c09f30ac080a
Username117-java <Tupak008@yandex.ru>
2025-04-03 11:57:46 +0300

    Initial Commit

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,39 @@
+package ru.hogwarts.school.service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+@Service
+public class FacultyService {
+
+    private FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty addFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty findFaculty(long id) {
+        return facultyRepository.findById(id).get();
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public Collection<Faculty> findByColor(String color) {
+        return facultyRepository.findByColor(color);
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,38 @@
+package ru.hogwarts.school.service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> findByAge(int age) {
+        return studentRepository.findByAge(age);
+    }
+}
